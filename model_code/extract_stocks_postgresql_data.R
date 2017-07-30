347a2286a1eaba787ae25c379371978682ecb6ec
skiptoniam <skiptoniam@gmail.com>
2017-07-30 20:05:08 +1000

    test

--- model_code/extract_stocks_postgresql_data.R
+++ model_code/extract_stocks_postgresql_data.R
@@ -7,4 +7,4 @@ chartSeries(CBA.AX,type = 'bars', theme = "black")
 candleChart(CBA.AX,subset='2016-12::2017', theme = chartTheme("white")) 
 
 get_symbols_postgreSQL('MSCI',user='woo457',password='pass', dbname='stocksdb')
-chartSeries(MSCI, , theme='white.mono',bar.type='hlc')
+chartSeries(MdSCI, , theme='white.mono',bar.type='hlc')
